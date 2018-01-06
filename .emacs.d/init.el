2fee9df062000f790b1b2448115acab80fa32da7
tsukudamayo <tsukudamayo@gmail.com>
2018-01-06 16:50:36 +0900

    add emacs jedi 20170106

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,22 +10,23 @@
 
 ;; settings backup directory
 (setq backup-directory-alist
-      (cons (cons ".*" (expand-file-name "~/.emacs.d/backup"))
-	    backup-directory-alist))
+  (cons (cons ".*" (expand-file-name "~/.emacs.d/backup"))
+        backup-directory-alist))
 (setq auto-save-file-name-transforms
-      `((".*", (expand-file-name "~/.emacs.d/backup/") t)))
+  `((".*", (expand-file-name "~/.emacs.d/backup/") t)))
 
 ;; python-mode settings
 (require 'python-mode)
-(autoload 'python-mode "python-mode" "Python Mode." t)
 (setq auto-mode-alist (cons '("\\.py\\'" . python-mode) auto-mode-alist))
+(setq interpreter-mode-alist (cons '("python" . python-mode) interpreter-mode-alist))
 
 ;; jedi settings
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
-;; settings chnage virtualenv
-(require 'virtualenvwrapper)
-(require 'auto-virtualenvwrapper)
-(add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+;; ;; settings chnage virtualenv
+;; (require 'virtualenvwrapper)
+;; (require 'auto-virtualenvwrapper)
+;; (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+
