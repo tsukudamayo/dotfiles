da4dee6db2768ccea738354071cfce7988f401db
tsukudamayo <tsukudamayo@gmail.com>
2018-01-16 22:48:49 +0900

    add .emacs.d/init.el change flymake-errline -> red flymake-warnline -> yellow , add markdown-mode 20180116

--- .emacs.d/elpa/concurrent-20161228.1930/concurrent-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; concurrent-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("concurrent.el") (23119 34312 456521 861000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; concurrent-autoloads.el ends here
--- .emacs.d/elpa/dash-20171028.854/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20171028.854" "A modern list library for Emacs" 'nil :commit "e9c792f8c407fe0b2190c9e2533bdd000d753f05" :keywords '("lists"))
