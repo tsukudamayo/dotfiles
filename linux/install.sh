57fa590c500d87da431f93bb10893df2ea5d387d
tsukudamayo <tsukudamayo@gmail.com>
2019-04-20 11:05:15 +0900

    add install.sh to install docker  command

--- linux/install.sh
+++ linux/install.sh
@@ -145,3 +145,19 @@ cargo install racer
 
 # c++ settings
 sudo apt-get install -y llvm-dev clang libclang-dev cmake
+
+# docker install
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
