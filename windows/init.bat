f6f4af7cb28bb6c1ca700ed7f04ebc1385b029a7
tsukudamayo <tsukudamayo@gmail.com>
2019-04-27 22:06:24 +0900

    change path which copy .emacs

--- windows/init.bat
+++ windows/init.bat
@@ -3,13 +3,55 @@ mkdir %HOMEPATH%\.vimfiles\undo
 mkdir %HOMEPATH%\.vimfiles\backup
 mkdir %HOMEPATH%\.vimfiles\swp
 
-copy %HOMEPATH%\dotfiles\.vimrc %HOMEPATH%\.vimrc
+mkdir %HOMEPATH%\AppData\Roaming\.emacs.d
+mkdir %HOMEPATH%\AppData\Roaming\.emacs.d\backup
 
-set PATH=C:\Users\USER\opt\emacs\bin;%PATH%
+copy %HOMEPATH%\dotfiles\.vimrc %HOMEPATH%\.vimrc
+copy %HOMEPATH%\dotfiles\windows\.emacs %HOMEPATH%\AppData\Roaming\.emacs
+
+:bin
+set PATH=%HOMEPATH%\bin;%PATH%
+
+:msys2
 set PATH=C:\msys64\mingw32\bin;%PATH%
 set PATH=C:\msys64\mingw64\bin;%PATH%
 set PATH=C:\msys64\usr\bin;%PATH%
-set PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319;%PATH%
-set PATH=C:\Users\USER\opt\vim\src;%PATH%
 
-C:\Users\USER\tools\"Microsoft Visual Studio"\2017\Community\VC\Auxiliary\Build\vcvarsall amd64
+:CMake
+set PATH=%HOMEPATH%\tools\CMake\bin;%PATH%
+
+:LLVM 
+:Clang
+set PATH=%HOMEPATH%\tools\LLVM\bin;%PATH%
+
+:ImageMagick
+set PATH=%HOMEPATH%\tools\ImageMagick;%PATH%
+
+:rust
+set PATH=%HOMEPATH%\.cargo\bin;%PATH%
+
+:NET
+set PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319;%PATH%
+
+:Java
+set PATH=%HOMEPATH%\tools\Java\jdk-10.0.1\bin;%PATH%
+set PATH=%HOMEPATH%\tools\apache-maven-3.5.3\bin;%PATH%
+set PATH=%HOMEPATH%\tools\gradle-4.7-all\gradle-4.7\bin;%PATH%
+echo %JAVA_HOME%
+
+:Go
+set PATH=%GOPATH%\bin;%PATH%
+set PATH=%HOMEPATH%\tools\Go\bin;%PATH%
+
+:editor
+set PATH=%HOMEPATH%\opt\emacs\bin;%PATH%
+set PATH=%HOMEPATH%\opt\vim\src;%PATH%
+
+:kubectl
+set PATH=%HOMEPATH%\tools\kubectl;%PATH%
+
+:node
+set PATH=%HOMEPATH%\AppData\Roaming\npm\node_modules;%PATH%
+
+:VisualStudio
+%HOMEPATH%\tools\"Microsoft Visual Studio"\2017\Community\VC\Auxiliary\Build\vcvarsall amd64
