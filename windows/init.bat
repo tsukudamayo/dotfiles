ff00b085bdc241900bc45d265a5bf43dfb868e18
tsukuda <v_mayo.tukuda@pactera.com>
2020-03-30 19:35:21 +0900

    pactera finish

--- windows/init.bat
+++ windows/init.bat
@@ -1,70 +1,146 @@
-mkdir %HOMEPATH%\vimfiles
-mkdir %HOMEPATH%\vimfiles\undo
-mkdir %HOMEPATH%\vimfiles\backup
-mkdir %HOMEPATH%\vimfiles\swp
+git config --global user.email "v_mayo.tukuda@pactera.com"
+git config --global user.name "tsukuda"
 
-mkdir %HOMEPATH%\AppData\Roaming\.emacs.d
-mkdir %HOMEPATH%\AppData\Roaming\.emacs.d\backup
+set PATH=%PATH%;%HOMEPATH%\bin
+set PATH=%PATH%;%HOMEPATH%\lib\tsukudaoriginal
 
-copy %HOMEPATH%\dotfiles\.vimrc %HOMEPATH%\.vimrc
-copy %HOMEPATH%\dotfiles\windows\.emacs %HOMEPATH%\AppData\Roaming\.emacs
+:golang
+set PATH=%PATH%;%HOMEPATH%\tools\Go\bin
+set PATH=%PATH%;%HOMEPATH%\go\bin
 
-:bin
-set PATH=%HOMEPATH%\bin;%PATH%
+:java
+set PATH=%PATH%;%HOMEPATH%\tools\Java\jdk1.8.0_231\bin
+:java_home
+set JAVA_HOME=C:\Users\tsukuda\tools\Java\jdk1.8.0_231
+:gradle
+set PATH=%PATH%;C:\Gradle\gradle-6.2.2\bin
 
-:msys2
-set PATH=C:\msys64\mingw32\bin;%PATH%
-set PATH=C:\msys64\mingw64\bin;%PATH%
-set PATH=C:\msys64\usr\bin;%PATH%
+:scala
+set PATH=%PATH%;%HOMEPATH%\tools\scala\bin
+:sbt
+set PATH=%PATH%;%HOMEPATH%\tools\sbt\bin
 
-:CMake
-set PATH=%HOMEPATH%\tools\CMake\bin;%PATH%
+:hadoop_spark
+set HADOOP_HOME=C:\Users\tsukuda\tools\spark-2.4.4-bin-hadoop2.7
+set SPARK_HOME=C:\Users\tsukuda\tools\spark-2.4.4-bin-hadoop2.7
+set PATH=%PATH%;C:\Users\tsukuda\tools\spark-2.4.4-bin-hadoop2.7\bin
+set PYSPARK_PYTHON=C:\Users\tsukuda\tools\Miniconda3\\envs\mongodb-sample\python.exe
+set PYSPARK_DRIVER_PYTHON=jupyter
+set PYSPARK_DRIVER_PYTHON_OPTS=notebook
+set PYSPARK_SUBMIT_ARGS="pyspark-shell"
 
-:LLVM 
-:Clang
-set PATH=%HOMEPATH%\tools\LLVM\bin;%PATH%
+:nodejs complete
+set PATH=%PATH%;%HOMEPATH%\tools\nodejs
+:yarn
+set PATH=%PATH%;%HOMEPATH%\tools\Yarn\bin
 
-:ImageMagick
-set PATH=%HOMEPATH%\tools\ImageMagick;%PATH%
+:ruby
+set PATH=%PATH%;%HOMEPATH%\tools\Ruby25-x64\bin
 
-:rust
-set PATH=%HOMEPATH%\.cargo\bin;%PATH%
+:perl
+set PATH=%PATH%;C:\Perl64\bin
 
-:NET
-set PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319;%PATH%
+:python requirements.txt
+set CONDA_FORCE_32BIT=
+copy %HOMEPATH%\dotfiles\windows\requirements.txt %HOMEPATH%\etc
 
-:Java
-set PATH=%HOMEPATH%\tools\Java\jdk-10.0.1\bin;%PATH%
-set PATH=%HOMEPATH%\tools\apache-maven-3.5.3\bin;%PATH%
-set PATH=%HOMEPATH%\tools\gradle-4.7-all\gradle-4.7\bin;%PATH%
-echo %JAVA_HOME%
-
-:Go
-set PATH=%GOPATH%\bin;%PATH%
-set PATH=%HOMEPATH%\tools\Go\bin;%PATH%
-
-:editor
-set PATH=%HOMEPATH%\opt\emacs\bin;%PATH%
-set PATH=%HOMEPATH%\opt\vim\src;%PATH%
-
-:kubectl
-set PATH=%HOMEPATH%\tools\kubectl;%PATH%
-
-:node
-set PATH=%HOMEPATH%\AppData\Roaming\npm\node_modules;%PATH%
-
-:redis
-set PATH=%HOMEPATH%\tools\Redis;%PATH%
-
-:hugo
-set PATH=%HOMEPATH%\tools\Hugo\bin;%PATH%
+:clisp
+set PATH=%PATH%;%HOMEPATH%\lib\roswell
 
 :R
-set PATH=%HOMEPATH%\tools\R-3.6.0\bin;%PATH%
-set PATH=%HOMEPATH%\tools\R-3.6.0\bin\x64;%PATH%
+set PATH=%PATH%;%HOMEPATH%\tools\R-3.6.0\bin\x64
 
 :julia
-set PATH=%HOMEPATH%\tools\Julia-1.1.0\bin;%PATH%
+set PATH=%PATH%;%HOMEPATH%\tools\Julia-1.1.0\bin
 
-:VisualStudio
-%HOMEPATH%\tools\"Microsoft Visual Studio"\2017\Community\VC\Auxiliary\Build\vcvarsall amd64
+:rust
+set PATH=%PATH%;%HOMEPATH%\.cargo\bin
+
+:make
+set PATH=%PATH%;%HOMEPATH%\tools\GnuWin32\bin
+
+
+:tools
+set PATH=%PATH%;%HOMEPATH%\tools\7-Zip
+set PATH=%PATH%;%HOMEPATH%\tools\rlogin
+
+:emacs
+set PATH=%PATH%;%HOMEPATH%\tools\emacs-26.1\bin
+
+:vim
+set PATH=%PATH%;%HOMEPATH%\tools\vim\src
+copy %HOMEPATH%\dotfiles\.vimrc %HOMEPATH%\
+
+:cmder config file
+copy %HOMEPATH%\dotfiles\windows\ConEmu.xml %HOMEPATH%\tools\cmder\vendor\conemu-maximus5
+
+:putty
+set PATH=%PATH%;%HOMEPATH%\tools\PuTTY
+
+:teraterm
+set PATH=%PATH%;%HOMEPATH%\tools\teraterm
+
+:cabocha
+set PATH=%PATH%;C:\Program Files (x86)\CaboCha\bin
+
+:juman
+set PATH=%PATH%;%HOMEPATH%\tools\juman
+
+:knp
+set PATH=%PATH%;%HOMEPATH%\tools\knp
+
+:mecab
+set PATH=%PATH%;C:\Program Files (x86)\MeCab\bin
+
+:llvm
+set PATH=%PATH%;%HOMEPATH%\tools\LLVM\bin
+
+:cmake
+set PATH=%PATH%;%HOMEPATH%\tools\CMake\bin
+
+:graphviz
+set PATH=%PATH%;%HOMEPATH%\tools\Miniconda3\envs\jpnlp32\Library\bin\graphviz
+
+:dockertoolbox
+rem set PATH=%PATH%;C:\"Program Files"\"Docker Toolbox"
+
+:docker_for_windows
+set PATH=%PATH%;C:\"Program Files"\Docker\Docker\resources\bin
+:virtualbox
+set PATH=%PATH%;C:\Program Files\Oracle\VirtualBox
+
+:kubectl
+set PATH=%PATH%;%HOMEPATH%\tools\kubectl
+
+:protoc
+set PATH=%PATH%;%HOMEPATH%\tools\protoc\bin
+
+:webprotoc
+set PATH=%PATH%;%HOMEPATH%\tools\web-grpc-1.0.6
+
+:ginza
+set PATH=%PATH%;%HOMEPATH%\tools\Miniconda3\envs\ginza\Lib\site-packages\ja_ginza
+
+:kytea
+set PATH=%PATH%;%HOMEPATH%\local\nlp_learning_for_python\recipe_nlp\kytea-win-0.4.2
+
+:openssl
+set PATH=%PATH%;%HOMEPATH%\tools\OpenSSL-Win32\bin
+
+:helm
+set PATH=%PATH%;%HOMEPATH%\tools\helm-v3.0.0-rc.1-windows-amd64\windows-amd64
+
+:WaImportExportV1
+set PATH=%PATH%;%HOMEPATH%\tools\WaImportExportV1
+
+:mongodb
+set PATH=%PATH%;C:\"Program Files"\MongoDB\Server\4.2\bin
+
+:mysql
+set PATH=%PATH%;C:\"Program Files"\MySQL\"MySQL Server 5.6"\bin
+
+:roswell
+set PATH=%PATH%;%HOMEPATH%\tools\roswell
+
+:msbuild
+C:\"Program Files (x86)"\"Microsoft Visual Studio"\2017\Community\VC\Auxiliary\Build\vcvars64.bat
