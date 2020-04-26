a1382f84d06dd431a6e14c04a3b1a9fae322369b
tsukudamayo <tsukudamayo@gmail.com>
2020-04-26 18:14:22 +0900

    python3.7 build.sh for Dockerfile corespondse mac and linux

--- docker/python-3.7/build.sh
+++ docker/python-3.7/build.sh
@@ -1,8 +1,16 @@
-xhost +local:
-docker run -it --rm \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=$DISPLAY \
-    python-3.7 \
-    /bin/bash
+#!/bin/bash
 
+xhost +local:
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        python3.7 \
+        /bin/bash
+else
+     docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$DISPLAY \
+        python3.7 \
+        /bin/bash
+fi
 xhost -local:
