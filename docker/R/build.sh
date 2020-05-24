9d5a15ef049db300c922aa8ef3a420d2bf6d82f8
tsukudamayo <tsukudamayo@gmail.com>
2020-05-25 03:46:18 +0900

    add docker/R

---
+++ docker/R/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+xhost +local:
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        r-dev \
+        /bin/bash
+else
+     docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$DISPLAY \
+        r-dev \
+        /bin/bash
+fi
+xhost -local:
