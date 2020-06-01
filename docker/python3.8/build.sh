d7630921e8b504091b9ab392a4ff375e34f36a5b
tsukudamayo <tsukudamayo@gmail.com>
2020-06-01 09:13:19 +0900

    docker/python3.7-8 using poetry

---
+++ docker/python3.8/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xhost +localhost:
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        python3.8 \
+        /bin/bash
+    xhost -localhost:
+else
+    xhost +local:
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$DISPLAY \
+        python3.8 \
+        /bin/bash
+    xhost -local:
+fi
