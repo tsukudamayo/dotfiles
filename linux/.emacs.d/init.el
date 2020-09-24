e9f066d3c01d13b708bbc0031f8aaaba2672eeaf
tsukudamayo <tsukudamayo@gmail.com>
2020-09-25 05:25:47 +0900

    set web-mode indent-tabs-mode nil and indent-offset 2

--- linux/.emacs.d/init.el
+++ linux/.emacs.d/init.el
@@ -356,6 +356,15 @@ locate PACKAGE."
 (require-package 'web-mode)
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-hook 'web-mode-hook
+	  '(lambda ()
+	     (setq web-mode-attr-indent-offset nil)
+	     (setq web-mode-markup-indent-offset 2)
+	     (setq web-mode-css-indent-offset 2)
+	     (setq web-mode-code-indent-offset 2)
+	     (setq web-mode-sql-indent-offset 2)
+	     (setq indent-tabs-mode nil)
+	     (setq tab-width 2)))
 
 ;; ;; vue-mode
 ;; (require-package 'vue-mode)
