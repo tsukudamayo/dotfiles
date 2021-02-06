5f460af6d3ea977d0d17f669a18752079cd9c12b
tsukudamayo <tsukudamayo@gmail.com>
2021-02-06 14:13:10 +0900

    add Dockerfile for stoplight-studio

---
+++ docker/stoplight/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xhost +localhost
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        stoplight-studio \
+        /bin/bash
+    xhost -localhost
+else
+    xhost +local:
+    docker run -it --rm \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=$DISPLAY \
+        stoplight-studio \
+        /bin/bash
+    xhost -local:
+fi
