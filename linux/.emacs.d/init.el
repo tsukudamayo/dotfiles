38a30448c4b7739ad057e3233cd0b95814376f03
tsukudamayo <tsukudamayo@gmail.com>
2021-07-27 16:05:20 +0900

    push changing .emacs for JavaScript/TypeScript lsp-mode

--- linux/.emacs.d/init.el
+++ linux/.emacs.d/init.el
@@ -349,7 +349,6 @@ locate PACKAGE."
      (flycheck-add-mode 'javascript-eslint 'js2-mode))
 )
 
-
 ;; typescript
 (require-package 'typescript-mode)
 (require 'typescript-mode)
@@ -365,6 +364,28 @@ locate PACKAGE."
 	    (eldoc-mode t)
 	    (company-mode-on)))
 (setq typescript-indent-level 4)
+
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode))
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+(load-theme 'zenburn t)
+;; (helm-mode)
+;; (require 'helm-xref)
+;; (define-key global-map [remap find-file] #'helm-find-files)
+;; (define-key global-map [remap execute-extended-command] #'helm-M-x)
+;; (define-key global-map [remap switch-to-buffer] #'helm-mini)
+(which-key-mode)
+(add-hook 'prog-mode-hook #'lsp)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      create-lockfiles nil) ;; lock files will kill `npm start'
+(with-eval-after-load 'lsp-mode
+  (require 'dap-chrome)
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (yas-global-mode))
 
 ;; web-mode
 (require-package 'web-mode)
