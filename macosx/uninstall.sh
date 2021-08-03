5278359e0216e5446f5484fa545c926cf047c228
tsukudamayo <tsukudamayo@gmail.com>
2021-08-03 09:48:25 +0900

    fix typo

---
+++ macosx/uninstall.sh
@@ -0,0 +1,13 @@
+rm -rf ~/dotfiles
+brew uninstall git
+brew uninstall git-flow
+brew cask uninstall docker
+brew uninstall docker-compose
+brew uninstall wget
+brew cask uninstall firefox
+brew cask uninstall xquartz
+brew uninstall tmux
+brew uninstall make
+brew uninstall cmake
+brew uninstall llvm
+brew uninstall pandoc
