76a2da07291433b4c74b6195690d9330b7fbaed3
tsukudamayo <tsukudamayo@gmail.com>
2021-11-07 02:25:19 +0000

    update terraform dev.Dockerfile

--- linux/.emacs.d/init.el
+++ linux/.emacs.d/init.el
@@ -617,7 +617,7 @@ locate PACKAGE."
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection
-				   '("/usr/local/bin/terraform-ls" "serve"))
+				   '("/usr/bin/terraform-ls" "serve"))
                   :major-modes '(terraform-mode)
                   :server-id 'terraform-ls))
 (add-hook 'terraform-mode-hook #'lsp)
