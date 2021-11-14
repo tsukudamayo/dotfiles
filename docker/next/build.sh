95a24286031f7b8ceb00fb08965a9849e5520aa1
tsukudamayo <tsukudamayo@gmail.com>
2021-11-14 09:53:24 +0900

    add next Dockerfile port 3000

--- docker/next/build.sh
+++ docker/next/build.sh
@@ -4,6 +4,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     xhost +localhost
     docker run -it --rm \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -p 3000:3000 \
         --name next-dev \
         next-dev \
         /bin/bash
@@ -12,6 +13,7 @@ else
     xhost +local:
     docker run -it --rm \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -p 3000:3000 \
         -e DISPLAY=$DISPLAY \
         --name next-dev \
         next-dev \
