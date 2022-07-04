7dc04264d99828457f7d9981cf8474e0247682f5
tsukudamayo <tsukudamayo@gmail.com>
2022-07-04 15:57:35 +0900

    change how to install homebrew to m1 mac

--- macosx/install-m1mac.sh
+++ macosx/install-m1mac.sh
@@ -15,7 +15,7 @@ mkdir ~/vimfiles/backup
 mkdir ~/vimfiles/undo
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew doctor
 
