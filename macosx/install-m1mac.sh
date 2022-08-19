00f93eee67c33083fe1d44bbbf4df0292cc15216
tsukudamayo <tsukudamayo@gmail.com>
2022-08-19 19:17:41 +0900

    delete comment out line in install script for m1 mac.

--- macosx/install-m1mac.sh
+++ macosx/install-m1mac.sh
@@ -72,20 +72,8 @@ mv docker/* $HOME/bin
 rm -rf docker/
 rm docker-20.10.17.tgz
 
-# # wget
-# brew install wget
-
 # tmux
 brew install tmux
-
-# # make
-# brew install make
-
-# # cmake
-# brew install cmake
-
-# # llvm
-# brew install llvm
 
 # emacs(native compile)
 brew install autoconf gnutls texinfo
@@ -110,104 +98,3 @@ multipass mount /private/tmp docker-vm:/tmp
 
 docker context create docker-vm --docker "host=tcp://$(multipass list | grep docker-vm | awk '{print $3}'):2375"
 docker context use docker-vm
-
-# # gcp sdk
-# curl https://sdk.cloud.google.com > gcp-sdk-install.sh
-# bash gcp-sdk-install.sh --disable-prompts
-# rm gpc-sdk-install.sh 
-
-## pandoc
-#brew install pandoc 
-
-## for gpu install(cuda, cudnn, bazel, coreutils)
-#brew install coreutils
-#brew install bazel
-#echo "cuDNN Library for OSX downloads and copy /usr/local/cuda"
-
-
-## vlc
-#brew install --cask vlc
-#
-## libreoffice
-#brew install --cask libreoffice
-#
-## lua
-#brew install lua
-#
-## TODO vim install error
-# brew install vim ¥
-# --with-lua
-# --with-luajit
-# --override-system-vi
-# --with-tcl
-
-## miniconda(python)
-#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-#chmod +x ~/miniconda.sh
-#bash ~/miniconda.sh -b -p $HOME/miniconda
-#source ~/miniconda/bin/activate
-#conda init
-
-## leveldb(for neo-python)
-#brew install leveldb
-#
-## sqlite
-#brew install sqlite3
-#
-## postgre
-#brew install postgresql
-#
-## go
-#brew install go
-#mkdir ~/go
-#echo 'export GOPATH=~/go' >> ~/.bash_profile
-#source ~/.bash_profile
-#chmod +x ~/dotfiles/go/go-get.sh
-#./dotfiles/go/go-get.sh
-#
-## nodejs
-#brew install anyenv
-#anyenv init
-#echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
-#exec $SHELL -l
-#anyenv install --force-init
-#anyenv install nodenv
-#
-#npm install -g tern
-#
-## common lisp
-#brew install roswell
-#ros setup
-#ros install clisp
-#ros install slime
-#
-## scheme
-#brew install --cask racket
-#raco pkg install sicp
-#
-## rust
-#curl https://sh.rustup.rs -sSf | sh
-#source $HOME/.cargo/env
-#cargo install rustfmt --force
-#rustup install nightly
-#cargo +nightly install racer --force
-#rustup component add rls-preview rust-analysis rust-src
-
-# git clone https://github.com/hugoduncan/flycheck-cargo.git
-# cp flycheck-cargo/flycheck-cargo.el ~/.emacs.d/elpa/
-# rm -rf flycheck-cargo
-# git clone https://github.com/rust-lang/rust.git ~/lib/src/rust
-
-## R
-#brew install r
-#brew install --cask rstudio
-#git clone https://https://github.com/myuhe/auto-complete-acr.el.git ~/dotfiles/.emacs.d/elpa
-#
-## julia
-#brew install --cask julia
-#
-## circleci/cli
-#curl -fLSs https://circle.ci/cli | bash
-#
-## stoplight-studio
-#brew install --cask stoplight-studio
