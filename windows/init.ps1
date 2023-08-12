40bfbc6ead48d29fb4530419f70b0360e6a75be8
tsukudamayo <tsukudamayo@gmail.com>
2023-08-12 10:05:56 +0900

    add about windows dev enviroment

--- windows/init.ps1
+++ windows/init.ps1
@@ -1,1 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install packages.config
+git config --global user.email tsukudamayo@gmail.com
+git config --global user.name tsukudamayo
+git config --global core.autocrlf true
+wsl --install -d Debian
