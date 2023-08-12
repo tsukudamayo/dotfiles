7a86f529eafc7c1393b196d4c1c7623d7694c7fa
tsukudamayo <tsukudamayo@gmail.com>
2023-08-12 11:41:58 +0900

    add docker cli and configure docker deamon in multipass for windows10-home

--- windows/init-multipass.ps1
+++ windows/init-multipass.ps1
@@ -1,6 +1,6 @@
-multipass launch --name docker-vm --cpus 8 --mem 16G --disk 200G --cloud-init $HOME/dotfiles/macosx/cloud-config-x86_64.yml 22.04
+multipass launch --name docker-vm --cpus 8 --mem 8G --disk 50G --cloud-init $HOME/dotfiles/windows/cloud-config-x86_64.yml 22.04
 multipass mount /Users docker-vm:/Users
 multipass mount /private/tmp docker-vm:/tmp
-
-docker context create docker-vm --docker "host=tcp://$(multipass list | grep docker-vm | awk '{print $3}'):2375"
+$multipass_ip =  multipass list | Out-String -Stream | Select-String "docker-vm" | Out-String -Stream | Select-String -Pattern "172.17.0.\d{1,3}" | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value}
+docker context create docker-vm --docker "host=tcp://${multipass_ip}:2375"
 docker context use docker-vm
